60cb9e5e0c48fc5e42a1d74d39c6763d2ff00028
Jessica Dunstan <jdunstan@cs.washington.edu>
2021-03-02 11:06:50 -0800

    Hackintosh utilities

--- custom_ohmyzsh/custom/start.zsh
+++ custom_ohmyzsh/custom/start.zsh
@@ -36,17 +36,22 @@ affirmations () {
     violet "I love myself. I love my life. The world is better with me in it."
 }
 
+
+##############################################
+#                OS Utilities                #
+##############################################
+
+
+DEVELOPER_DIRECTORY=$HOME/Developer
+GITHUB_DIRECTORY=$DEVELOPER_DIRECTORY/github
+
 isLinux () {
     [ $(uname -s ) = "Linux" ]
 }
 
 ##############################################
-#                Productivity                #
+#                Mac-To-Linux                #
 ##############################################
-
-alias c=clear
-
-EDITOR=code
 
 ## MacOS-like aliases for Linux.
 if [ $(isLinux) ]; then
@@ -55,36 +60,103 @@ if [ $(isLinux) ]; then
     alias pbpaste='xclip -selection clipboard -o'
 fi
 
+# # Allow Touch-ID authentication for sudo.
+sudo () {
+    unset -f sudo
+    if [[ "$(uname)" == 'Darwin' ]] && ! grep 'pam_tid.so' /etc/pam.d/sudo --silent; then
+      sudo sed -i -e '1s;^;auth       sufficient     pam_tid.so\n;' /etc/pam.d/sudo
+    fi
+    sudo "$@"
+  }
+
+##############################################
+#                Hackintosh                  #
+##############################################
+
+HACKINTOSH_DIR=$GITHUB_DIRECTORY/hackintosh/
+
+# Get the Mount EFI command.
+MOUNT_EFI_COMMAND=$HACKINTOSH_DIR/MountEFI.command
+if [ -x  MOUNT_EFI_COMMAND ]; then
+    alias mountefi=$MOUNT_EFI_COMMAND
+fi
+
+
+##############################################
+#                Productivity                #
+##############################################
+
+alias c=clear
+
+export EDITOR="$(whence code) --new-window --wait"
+
 # To stop  ZSH auto-correcting i.e. [nyae]
 alias test="nocorrect test"
 
+# Chezmoi does not allow git submodules, so we have to manually update Oh My ZSH, as well as its plugins and themes.
+chezmoi_update_all () {
+    # Update Oh My ZSH
+    OH_MY_ZSH_GITHUB_SOURCE=https://github.com/robbyrussell/oh-my-zsh/archive/master.tar.gz
+    OH_MY_ZSH_DEST=/tmp/oh-my-zsh-master.tar.gz
+    touch $OH_MY_ZSH_DEST
+    echo "Donwloading Oh my zsh from $OH_MY_ZSH_GITHUB_SOURCE"
+    curl -L -o $OH_MY_ZSH_DEST  $OH_MY_ZSH_GITHUB_SOURCE&& \
+        chezmoi import --strip-components 1 --destination ${HOME}/.oh-my-zsh $OH_MY_ZSH_DEST
+
+    # Update Powerlevel 10K Theme
+    P10K_GITHUB_SOURCE=https://github.com/romkatv/powerlevel10k/archive/master.tar.gz
+    P10K_DEST=/tmp/p10k.tar.gz
+    touch $P10K_DEST
+    echo "Donwloading P10K from $P10K_GITHUB_SOURCE"
+    curl -L -o $P10K_DEST $P10K_GITHUB_SOURCE && \
+        chezmoi import --strip-components 1 --destination $ZSH_CUSTOM/themes/ $P10K_DEST
+
+    # Update Nix shell plugin
+    ZSH_NIX_SHELL_GITHUB_SOURCE=https://github.com/chisui/zsh-nix-shell/archive/master.tar.gz
+    ZSH_NIX_SHELL_DEST=/tmp/zsh-nix-shell.tar.gz
+    touch $ZSH_NIX_SHELL_DEST
+    echo "Donwloading P10K from $ZSH_NIX_SHELL_GITHUB_SOURCE"
+    curl -L -o $ZSH_NIX_SHELL_DEST $ZSH_NIX_SHELL_GITHUB_SOURCE && \
+        chezmoi import --strip-components 1 --destination $ZSH_CUSTOM/plugins/ $ZSH_NIX_SHELL_DEST
+}
+
+# Quickly edit your dotfiles.
 zshrc () {
     ZSHRC="$HOME/.zshrc"
     POWERLEVEL_10K_ZSH="$HOME/.p10k.zsh"
     ZSH_CUSTOM_RC="$ZSH_CUSTOM/start.zsh"
-    code --new-window -w "$ZSH_CUSTOM_RC" "$POWERLEVEL_10K_ZSH" "$ZSHRC" && \
-        purple "Finished editing, source'ing ~/.zshrc..." \
-        && . $ZSHRC
+    NIX_GOODIES="$HOME/.ALL_THE_GOODIES.nix"
+
+    FILES=($ZSH_CUSTOM_RC $POWERLEVEL_10K_ZSH $NIX_GOODIES $ZSHRC)
+    if [ -x $(whence chezmoi) ]; then
+        purple "Using Chezmoi"
+        chezmoi edit --prompt --apply $FILES
+    else
+        # Don't have Chezmoi installed, just edit things.
+        code --new-window -w $FILES && \
+            purple "Finished editing, source'ing ~/.zshrc..." \
+            && . $ZSHRC
+    fi
 }
 
 poretitioner () {
     pushd $HOME/Developer/poretitioner
 }
 
-# gitlab-runner () {
-#     # -d --rm -t -i
-#     docker run gitlab/gitlab-runner $@
+# # gitlab-runner () {
+# #     # -d --rm -t -i
+# #     docker run gitlab/gitlab-runner $@
+# # }
+# # -p "${GITLAB_RUNNER_BUILD_PORT}:${GITLAB_RUNNER_BUILD_PORT}" 
+
+# gitlab-runner-register () {
+#     docker run -it -v gitlab-runner-config:/etc/gitlab-runner/config.toml gitlab/gitlab-runner:latest register --run-untagged --config "$RUNNER_GITLAB_CONFIG_PATH"   --non-interactive  --url "https://gitlab.cs.washington.edu/" --tag-list ubuntu,jdunstan,vm,jdunstan-vm,ubuntu-vm --executor "docker" --docker-image "nixos/nix:latest" $@
+#   # --registration-token "hACNWtkLWLtksyWsP8Cv" \
 # }
-# -p "${GITLAB_RUNNER_BUILD_PORT}:${GITLAB_RUNNER_BUILD_PORT}" 
 
-gitlab-runner-register () {
-    docker run -it -v gitlab-runner-config:/etc/gitlab-runner/config.toml gitlab/gitlab-runner:latest register --run-untagged --config "$RUNNER_GITLAB_CONFIG_PATH"   --non-interactive  --url "https://gitlab.cs.washington.edu/" --tag-list ubuntu,jdunstan,vm,jdunstan-vm,ubuntu-vm --executor "docker" --docker-image "nixos/nix:latest" $@
-  # --registration-token "hACNWtkLWLtksyWsP8Cv" \
-}
-
-gitlab-runner-init () {
-    docker --debug run -p "$RUNNER_GITLAB_PORT_ROUTING_ARG" --name gitlab-runner --volume /var/run/docker.sock:/var/run/docker.sock --volume gitlab-runner-config:$RUNNER_GITLAB_CONFIG_PATH gitlab/gitlab-runner:latest --debug $@
-}
+# gitlab-runner-init () {
+#     docker --debug run -p "$RUNNER_GITLAB_PORT_ROUTING_ARG" --name gitlab-runner --volume /var/run/docker.sock:/var/run/docker.sock --volume gitlab-runner-config:$RUNNER_GITLAB_CONFIG_PATH gitlab/gitlab-runner:latest --debug $@
+# }
 
 
 kill-debugger () {
@@ -111,7 +183,6 @@ main () {
     #affirmations
     #poretitioner
     #prompt_nix_shell_setup "$@"
-
 }
-#POWERLEVEL10K_LEFT_PROMPT_ELEMENTS=(nix_shell)
+
 main
